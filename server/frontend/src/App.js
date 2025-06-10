a789c84c65970d3cbee203fd9d5be1d641f803e0
ak0402 <ak04_02@outlook.com>
2025-06-10 09:04:07 -0400

    after lab2

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,15 @@
 import LoginPanel from "./components/Login/Login"
-import { Routes, Route } from "react-router-dom";
+import Register from "./components/Register/Register";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<LoginPanel />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPanel />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-export default App;
+
